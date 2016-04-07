1647bc1d3c3e1de757532bd8d0346c91b7493a16
lyarmark <student1@student.touro.edu>
2016-04-07 19:44:29 -0400

    added logging to Paint

--- src/main/java/yarmark/paint/LineTool.java
+++ src/main/java/yarmark/paint/LineTool.java
@@ -1,10 +1,13 @@
 package yarmark.paint;
 
-import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.util.logging.Logger;
 
 public class LineTool extends Tool {
 
+	// every class gets its own logger
+	// param should be a unique name associated with the class
+	private static final Logger LOG = Logger.getLogger(LineTool.class.getName());
 	private int x1;
 	private int y1;
 	private int x2;
@@ -37,6 +40,16 @@ public class LineTool extends Tool {
 	@Override
 	public void drawPreview(Graphics2D g) {
 		g.setColor(properties.getColor());
+		// deliberate mistake to show logging
+		// testing this with the debugger interrupts it
+		// g.drawLine(x1, y1, x2, x2);
+		// System.out.println(String.format("%d %d %d %d", x1, y1, x2, y2));
+
 		g.drawLine(x1, y1, x2, y2);
+
+		String logMessage = (String.format("%d %d %d %d", x1, y1, x2, y2));
+		LOG.fine(logMessage);
+		// different levels of logging
+		// https://docs.oracle.com/javase/6/docs/api/java/util/logging/Level.html
 	}
 }
--- src/main/java/yarmark/paint/PaintFrame.java
+++ src/main/java/yarmark/paint/PaintFrame.java
@@ -2,6 +2,13 @@ package yarmark.paint;
 
 import java.awt.BorderLayout;
 import java.awt.Container;
+import java.io.IOException;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.FileHandler;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 import javax.inject.Singleton;
 import javax.swing.JFrame;
@@ -28,7 +35,27 @@ public class PaintFrame extends JFrame {
 		container.add(toolbar, BorderLayout.NORTH);
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SecurityException, IOException {
+
+		// this should change all loggers in app to display messages from all
+		// loggers fine and higher
+		// passing in package to affect all
+		// normal setting is info and above
+		Logger logger = Logger.getLogger("yarmark.paint");
+		logger.setLevel(Level.FINE);
+		// handler handles where the messages are sent
+		// can be logged to file, database
+		// java also has email API to send emails about severe errors
+
+		// Handler handler = new ConsoleHandler();
+
+		Handler handler = new FileHandler("log.txt"); // logs to file in xml
+		handler.setFormatter(new SimpleFormatter()); // to have it log in
+														// strings, same as to
+														// console
+		handler.setLevel(Level.FINE);
+		logger.addHandler(handler);
+
 		Injector injector = Guice.createInjector(new PaintModule());
 		PaintFrame frame = injector.getInstance(PaintFrame.class);
 		frame.setVisible(true);
